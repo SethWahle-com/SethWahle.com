34315132b5017ba39c53716aeae57990271392d5
Seth Wahle <osteth@gmail.com>
2025-03-30 14:44:26 -0500

    commit user-data update

--- user-data.sh
+++ user-data.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
 sudo apt-get update -y && sudo apt-get upgrade -y \
+sudo apt install nginx -y \
+sudo systemctl enable nginx \
+sudo systemctl start nginx \
+
+sudo git clone https://github.com/SethWahle-com/SethWahle.com.git /var/www/sethwahle.com \
+sudo chown -R $USER:$USER /var/www/sethwahle.com \  
+sudo chmod -R 755 /var/www/sethwahle.com/www/ \
+sudo tee /etc/nginx/sites-available/sethwahle.com > /dev/null << 'EOL'
+server {
+    listen 80;
+    listen [::]:80;
+    server_name sethwahle.com www.sethwahle.com;
+    root /var/www/sethwahle.com/webroot/RPG/www;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+EOL
+sudo ln -sf /etc/nginx/sites-available/sethwahle.com /etc/nginx/sites-enabled/ \
+sudo rm /etc/nginx/sites-enabled/default \
+
+sudo ufw allow 'Nginx Full' \
+sudo ufw allow 'Nginx HTTP' \
+sudo ufw allow 'Nginx HTTPS' \
+sudo ufw allow 'OpenSSH' \
+sudo ufw enable
+sudo nginx -t \
+sudo systemctl reload nginx \
