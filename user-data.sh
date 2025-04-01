dc1732fbe39687c0a110e966e3cd9f3815b5e814
Seth Wahle <osteth@gmail.com>
2025-04-01 00:37:12 -0500

    cleanup

--- user-data.sh
+++ user-data.sh
@@ -9,13 +9,13 @@ sudo systemctl start nginx
 
 sudo git clone https://github.com/SethWahle-com/SethWahle.com.git /var/www/sethwahle.com
 sudo chown -R $USER:$USER /var/www/sethwahle.com
-sudo chmod -R 755 /var/www/sethwahle.com/webroot/RPG/Website/www
+sudo chmod -R 755 /var/www/sethwahle.com/wwww
 
 sudo echo "server {" > /etc/nginx/sites-available/sethwahle.com
 sudo echo "    listen 80;" >> /etc/nginx/sites-available/sethwahle.com
 sudo echo "    listen [::]:80;" >> /etc/nginx/sites-available/sethwahle.com
 sudo echo "    server_name sethwahle.com www.sethwahle.com;" >> /etc/nginx/sites-available/sethwahle.com
-sudo echo "    root /var/www/sethwahle.com/webroot/RPG/Website/www;" >> /etc/nginx/sites-available/sethwahle.com
+sudo echo "    root /var/www/sethwahle.com/www;" >> /etc/nginx/sites-available/sethwahle.com
 sudo echo "    index index.html;" >> /etc/nginx/sites-available/sethwahle.com
 sudo echo "" >> /etc/nginx/sites-available/sethwahle.com
 sudo echo "    location / {" >> /etc/nginx/sites-available/sethwahle.com
